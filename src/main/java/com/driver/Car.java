11beabf1bb650d61143587bffaa696818d93bff7
ASUS <aryaprasharj02@gmail.com>
2023-01-21 20:46:18 +0530

    Hopping for last commit

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -71,3 +71,4 @@ public class Car extends Vehicle {
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
+
