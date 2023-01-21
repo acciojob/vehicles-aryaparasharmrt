9eb368137b2e5b831eb530d292818a604dd5d19c
ASUS <aryaprasharj02@gmail.com>
2023-01-21 20:35:34 +0530

    Solution 5

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,18 +1,23 @@
 package com.driver;
-
 public class Boat implements WaterVehicle{
-
     String name;
     int capacity;
-
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+  /*  public void setName(String name) {
+        this.name = name;
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }*/
     @Override
     public String getVehicleName() {
-        return name;
+        return this.name;
     }
-
     @Override
     public int getVehicleCapacity() {
-        return capacity;
+        return this.capacity;
     }
 }
-
