7ebfab25440e5fd82b98cc33c210acaccc31985d
ASUS <aryaprasharj02@gmail.com>
2023-01-21 20:37:19 +0530

    Solution 6

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -6,12 +6,12 @@ public class Boat implements WaterVehicle{
         this.name = name;
         this.capacity = capacity;
     }
-  /*  public void setName(String name) {
+    public void setName(String name) {
         this.name = name;
     }
     public void setCapacity(int capacity) {
         this.capacity = capacity;
-    }*/
+    }
     @Override
     public String getVehicleName() {
         return this.name;
